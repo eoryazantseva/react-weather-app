e01d3681bbb34c60749d3fe950561d561666b03f
eoryazantseva <eoryazantseva@gmail.com>
2022-03-29 14:01:16 +1000

    added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        This project was coded be Evgeniya Ryazantseva and is{" "}
+        <a
+          href="https://github.com/eoryazantseva/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
