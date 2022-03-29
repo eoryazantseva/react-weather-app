48a3fea445d9273c232f10c6415da6da964e8d61
eoryazantseva <eoryazantseva@gmail.com>
2022-03-29 22:26:27 +1000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        This project was coded be Evgeniya Ryazantseva and is{" "}
-        <a
-          href="https://github.com/eoryazantseva/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <footer>
+          This project was coded be Evgeniya Ryazantseva and is{" "}
+          <a
+            href="https://github.com/eoryazantseva/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
